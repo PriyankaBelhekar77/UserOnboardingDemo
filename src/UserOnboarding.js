5f4443c681cee8710776fd6131f3a9ede8fdf570
Priyanka_Belhekar <priyanka.belhekar@1silverbullet.tech>
2023-03-02 19:47:14 +0530

    feat: changes path

--- src/UserOnboarding.js
+++ src/UserOnboarding.js
@@ -12,10 +12,14 @@ class UserOnboarding {
     iframe.style.position = "fixed";
     iframe.style.top = "0";
     iframe.style.left = "0";
-    iframe.style.width = "100vw";
+    iframe.style.width = "50%";
     iframe.style.height = "100vw";
     iframe.style.border = "none";
     iframe.style.backgroundColor = "#FFFFFF";
+    iframe.style.display = "flex";
+    iframe.style.justifyContent = "center";
+    iframe.style.alignItems = "center";
+    iframe.style.boxShadow = "0 4px 8px 0 rgb(0 0 0 / 20%), 0 6px 20px 0 rgb(0 0 0 / 19%)";
 
     this.#iFrame = iframe;
     return iframe;
