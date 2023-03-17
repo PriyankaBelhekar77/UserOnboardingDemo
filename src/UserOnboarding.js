6641a16626b8a5e7b2a7815277a03b659357f93d
Priyanka_Belhekar <priyanka.belhekar@1silverbullet.tech>
2023-03-17 15:51:12 +0530

    feat: const change

--- src/UserOnboarding.js
+++ src/UserOnboarding.js
@@ -1,4 +1,22 @@
-import { VALIDATE_ARGS } from "../constants";
+// import { VALIDATE_ARGS } from "../constants";
+
+const VALIDATE_ARGS = {
+  domainLink: {
+    argName: 'domainLink',
+    regex: DOMAIN_REGEX,
+    error: '[1SilverBullet][user-onboarding] URL link provided is invalid. Please pass correct URL'
+  },
+  accessToken: {
+    argName: 'accessToken',
+    regex: '^[0-9a-fA-F]{64}$',
+    error: '[1SilverBullet][user-onboarding] Access token provided is invalid.'
+  },
+  // onClose: {
+  //   argName: 'onClose',
+  //   type: 'function',
+  //   error: '[1SilverBullet][user-onboarding] onClose callback provided is invalid. Please pass a function only'
+  // }
+};
 class UserOnboarding {
   #iFrame = document.createElement("iframe");
 
